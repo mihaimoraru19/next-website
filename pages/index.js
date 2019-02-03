72297f587fb2557131a196223f8d3704af1f548b
afmihai <mihaimx19@gmail.com>
2019-02-03 12:04:31 +0200

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import '../styles/style.scss'
+
+export default class Home extends Component {
+    render () {
+        return (
+            <div>
+                <Head />
+                <div>
+                    Test
+                </div>
+            </div>
+        )
+    }
+}
